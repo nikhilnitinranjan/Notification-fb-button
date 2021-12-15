dbcc61c337f7f48a1b8eb757c3f4fe317a3a2b25
Nikhil Nitin Ranjan <72209132+nikhilnitinranjan@users.noreply.github.com>
2021-12-15 18:37:23 +0530

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,28 @@
+var notify = document.querySelector('.notify');
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', active);
+
+function active(){
+	var add = Number(notify.getAttribute('data-count'))|| 0;
+	notify.setAttribute('data-count', add + 1);
+	if (add === 0) {
+		notify.classList.add('add-numb');
+	}
+	false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// linear-gradient(#ff4d4d,#ff3333,#ff1a1a);
